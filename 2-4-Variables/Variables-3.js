ef140fb682d78c1e78df6b9da58bbf0e1ad99df1
imamfarisi19 <edwardindo1@gmail.com>
2020-12-23 01:36:15 +0800

    working-with-variables exercise

--- 2-4-Variables/Variables-3.js
+++ 2-4-Variables/Variables-3.js
@@ -2,9 +2,9 @@ let hello = 'Hello World!';
 
 let message;
 
-// copy 'Hello World' from hello into message
+// copy 'Hello World' from hello into message 
 message = hello;
 
-// now two variables hold the same data
-alert(hello); // Hello World!
+// now two variables hold the same data 
+alert(hello); // Hello World! 
 alert(message); // Hello World!
