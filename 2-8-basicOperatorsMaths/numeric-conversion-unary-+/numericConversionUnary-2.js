6ac06aecf4c160984e3d97c5b20c91fe53031a56
imamfarisi19 <edwardindo1@gmail.com>
2021-01-08 23:40:02 +0800

    if we want to treat them as numbers we need to convert and then sum them

---
+++ 2-8-basicOperatorsMaths/numeric-conversion-unary-+/numericConversionUnary-2.js
@@ -0,0 +1,8 @@
+let apples = "2";
+let oranges = "3";
+
+//both values converted to numbers before the binary plus
+alert(+apples + +oranges); // 5
+
+// the longer variant
+// alert( Number(apples) + Number(oranges) ); // 5
