aafd07882d1bbcba8f14c61d0d884b9cf3b47315
imamfarisi19 <edwardindo1@gmail.com>
2021-01-14 03:51:45 +0800

    A regular equality check == has a problem. It cannot differentiate 0 from false

---
+++ 2-9-comparisons/strict-equality/strictEquality.js
@@ -0,0 +1,8 @@
+// A regular equality check == has a problem. It cannot differentiate 0 from false
+
+alert( 0 == false ); // true
+
+// The same thing happens with an empty string
+
+alert('' == false); // true
+
