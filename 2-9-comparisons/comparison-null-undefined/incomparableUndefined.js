e61c22d373ba0ae9ace16daa2ed53540e13a6565
imamfarisi19 <edwardindo1@gmail.com>
2021-01-15 00:36:27 +0800

    the value undefined shouldn

---
+++ 2-9-comparisons/comparison-null-undefined/incomparableUndefined.js
@@ -0,0 +1,9 @@
+alert(undefined > 0); // false (1)
+alert(undefined < 0); // false (2)
+alert(undefined == 0); // false (3)
+
+/* We get these result because:
+ * * Comparisons (1) and (2) return false bacause undefined gets converted to NaN and NaN is a special numeric value which returns false for all comparisons.
+ * * The equality check (3) returns false because undefined only equals null, undefined and no other value.
+ * */
+
