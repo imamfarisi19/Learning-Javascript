aa1c3792794a6f9e48cc276114ced12fed814982
imamfarisi19 <edwardindo1@gmail.com>
2021-01-16 01:22:29 +0800

    Here is how this looks using if..else

---
+++ 2-10-conditional-branching-if/multiple-question-mark/multipleQM-1.js
@@ -0,0 +1,12 @@
+if (age < 3) {
+  message = 'Hi, baby!';
+}
+else if (age < 18) {
+  message = 'Hello!';
+}
+else if (age < 100) {
+  message = 'Greetings!';
+}
+else {
+  message = 'What an unusual age!';
+}
