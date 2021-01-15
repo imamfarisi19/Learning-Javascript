d1bc52cae1a603d30c9e71655c1f65358dce4299
imamfarisi19 <edwardindo1@gmail.com>
2021-01-16 00:48:09 +0800

    but parentheses make the code more readable, so we recommend using them.

---
+++ 2-10-conditional-branching-if/conditional-operator/conditionalOperator-1.js
@@ -0,0 +1,9 @@
+// let result = condition ? value1 : value2;
+
+let accessAllowed = (age > 18) ? true : false;
+
+// the comparison operator "age > 18" executes first anyway
+// (no need to wrap it into parentheses)
+let accessAllowed = age > 18 ? true : false;
+
+// But parentheses make the code more readable, so we recommend using them. 
