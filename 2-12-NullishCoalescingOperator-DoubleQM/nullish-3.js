97035780aa49186905dfa4fd25d3aefbf6e081ab
imamfarisi19 <edwardindo1@gmail.com>
2021-01-28 20:57:17 +0800

    shows the first defined value

---
+++ 2-12-NullishCoalescingOperator-DoubleQM/nullish-3.js
@@ -0,0 +1,7 @@
+let firstName = null;
+let lastName = null;
+let nickName = "Supercoder";
+
+// shows the first defined value:
+alert(firstName ?? lastName ?? nickName ?? "Anonymous"); // Supercoder
+
