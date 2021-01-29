11b68fd514ed4a0c936a4cbf6808d174c3a55756
imamfarisi19 <edwardindo1@gmail.com>
2021-01-30 00:43:27 +0800

    here's exactly what happens in out case

---
+++ 2-13-loops-while-and-for/the-for-loop/for-2.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i < 3; i++) alert(i)
+
+// run begin
+let i = 0;
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// if condition -> run body and run step
+if (i < 3) { alert(i); i++ }
+// ... finish, because now i == 3
+
