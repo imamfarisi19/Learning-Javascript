a4a2023c4fd675565a29d3b2f51a9f258fcea58a
imamfarisi19 <edwardindo1@gmail.com>
2021-01-31 03:13:55 +0800

    sometimes we need to break out from multiple nested loops at once

---
+++ 2-13-loops-while-and-for/labels-for-break-continue/labels.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 3; i++) {
+
+  for (let j = 0; j < 3; k++) {
+
+    let input = prompt(`Value at coords (${i},${j})`,'');
+
+    // what if we want to exit from here to Done (below)?
+  }
+}
+
+alert('Done!');
+
