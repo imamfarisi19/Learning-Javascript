e6eae01e518961f02b8e470e6f36cef3d87c31a9
imamfarisi19 <edwardindo1@gmail.com>
2021-02-02 03:00:10 +0800

    the switch has one or more 'case' blocks and an optional default.

---
+++ 2-14-the-switch-statement/the-syntax/theSyntax.js
@@ -0,0 +1,14 @@
+switch(x) {
+  case 'value1': // if (x === 'value1')
+    ...
+    [break]
+
+  case 'value2': // if (x === 'value2')
+    ...
+    [break]
+	
+  default:
+    ...
+    [break]
+}
+
