99313ff98ad757db5bc6862e41f420e3ea6b013d
imamfarisi19 <edwardindo1@gmail.com>
2021-02-06 04:05:28 +0800

    If a same-named variable is declared inside the function then it shadows the outer one.

--- 2-15-functions/outer-variables/outerVariable-2.js
+++ 2-15-functions/outer-variables/outerVariable-2.js
@@ -11,4 +11,5 @@ function showMessage()
 // the function will create and use its own userName
 showMessage();
 
-alert( userName ); // John, unchanged, the function did not access the outer variable 
+alert( userName ); // John, unchanged, the function did not access the outer variable  
+
