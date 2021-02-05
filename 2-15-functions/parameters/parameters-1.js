6e7dc68e284a9c93b9cdc85d965e4224f224084e
imamfarisi19 <edwardindo1@gmail.com>
2021-02-06 04:28:36 +0800

    we have a variable from and pass it to the function, Please note: the function changes from, but the change is not seen outside, because a function always gets a copy of the value.

---
+++ 2-15-functions/parameters/parameters-1.js
@@ -0,0 +1,13 @@
+function showMessage(from, text) {
+	from = '*' + from + '*'; // make "from" look nicer 
+
+	alert( from + ': ' + text );
+}
+
+let from = "Ann";
+
+showMessage(from, "Hello"); // *Ann*: Hello 
+
+// the value of "from" is the same, the function modified a local copy
+alert ( from ); // Ann
+
