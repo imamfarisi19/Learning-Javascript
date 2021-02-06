0f52d6b4d26ad3aac623f0ad9152c5e02d75e9dd
imamfarisi19 <edwardindo1@gmail.com>
2021-02-07 03:02:27 +0800

    It is a widespread practice to start a function with a verbal prefix which vaguely describes the action. There must be an agreement within the team on the meaning of the prefixes.

---
+++ 2-15-functions/naming-a-function/naming.js
@@ -0,0 +1,5 @@
+showMessage(..)     // shows a message 
+getAge(..) 	    // returns the age (gets it somehow) 
+calcSum(..) 	    // calculates a sum and returns the result 
+createForm(..)      // creates a form (and usually returns it)
+checkPermission(..) // checks a permission, returns true/false
