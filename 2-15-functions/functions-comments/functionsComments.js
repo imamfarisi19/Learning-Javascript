24cc00169ad232602eacd91cf0e7e98be83e24d4
imamfarisi19 <edwardindo1@gmail.com>
2021-02-07 22:52:41 +0800

    the first variant uses a label

---
+++ 2-15-functions/functions-comments/functionsComments.js
@@ -0,0 +1,11 @@
+function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+    
+    for (let j = 2; j < i; j++) {
+      if (i%j==0) continue nextPrime;
+    }
+
+    alert(i); // a prime
+  }
+}
+
