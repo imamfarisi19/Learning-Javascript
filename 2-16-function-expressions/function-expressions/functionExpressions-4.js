bbb32268bf69f270200e73e8eb66c0321771868d
imamfarisi19 <edwardindo1@gmail.com>
2021-02-08 03:05:32 +0800

    Why is there a semicolon at the end ?

---
+++ 2-16-function-expressions/function-expressions/functionExpressions-4.js
@@ -0,0 +1,17 @@
+function sayHi() {
+  // ...
+}
+
+let sayHi = function() {
+ // ...
+}
+
+/* There's no need for ';' at the end of code blocks and syntax structures that use them like 'if { ... }', 'for { }'. 'function f { }' etc.
+ * */
+
+/* a function expression is used inside the statement.
+ * Let sayHi = ...; as a value. 
+ * It's note a code block, but rather an assigment.
+ * The semicolon ';' is recommended at the end of statements, no matter what the value is. 
+ * So the semicolon here is not related to the Function Expression itself, it just terminates the statement.
+ * */
