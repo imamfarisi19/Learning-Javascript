d53df9f60f23af2dde7eb1732649fff7068b130f
imamfarisi19 <edwardindo1@gmail.com>
2021-02-09 04:18:16 +0800

    The correct approach would be to a function Expression and assign 'welcome' to the variable that is declared outside of 'if' and has the proper visibility

---
+++ 2-16-function-expressions/function-expression-vs-function-declaration/functionExpression-2.js
@@ -0,0 +1,19 @@
+let age = prompt("What is your age?", 18);
+
+let welcome;
+
+if (age < 18) {
+
+  welcome = function() {
+    alert("Hello!");
+  };
+
+} else {
+
+  Welcome = function() {
+  alert("Greetings!");
+  };
+
+}
+
+welcome(); // ok now 
