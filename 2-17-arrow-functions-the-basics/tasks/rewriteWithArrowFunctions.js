0123a31c203112465388d6e3f6c748c350f2b505
imamfarisi19 <edwardindo1@gmail.com>
2021-02-10 06:01:01 +0800

    Replace Function Expressions with arrow functions in the code

---
+++ 2-17-arrow-functions-the-basics/tasks/rewriteWithArrowFunctions.js
@@ -0,0 +1,11 @@
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("you agreed."); 
+  () => alert("Greetings!");
+  
+);
