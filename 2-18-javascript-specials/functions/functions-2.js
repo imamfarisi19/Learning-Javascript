2cc610902b070e3f4598aa7c4f323e4caba134a8
imamfarisi19 <edwardindo1@gmail.com>
2021-02-11 00:28:28 +0800

    Arrow functions

---
+++ 2-18-javascript-specials/functions/functions-2.js
@@ -0,0 +1,15 @@
+// expression at the right side 
+let sum = (a, b) => a + b; 
+
+// or multi-line syntax with { ... }, need return here: 
+let sum = (a, b) => {
+  // ...
+  return a + b;
+}
+
+// without arguments 
+let sayHi = () => alert("Hello"); 
+
+// with a single argument
+let double = n => n * 2;
+
