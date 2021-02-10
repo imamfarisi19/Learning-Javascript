e0d4c54a36fb8b4f3fdd94bfc02131fcc9ddb47e
imamfarisi19 <edwardindo1@gmail.com>
2021-02-11 00:16:53 +0800

    All these functions are modal, they pause the code execution and prevent the visitor from interacting with the page until they answer

---
+++ 2-18-javascript-specials/interaction/interaction.js
@@ -0,0 +1,6 @@
+let userName = prompt("Your name?", "Alice"); 
+let isTeaWanted = confirm("Do you wnat some tea?"); 
+
+alert( "Visitor: " + userName ); // Alice 
+alert( "Tea Wanted: " + isTeaWanted ); // true
+
