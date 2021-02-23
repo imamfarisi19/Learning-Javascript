803aa9fb81e5677a9aa3c44c3a330972ff783c3c
imamfarisi19 <edwardindo1@gmail.com>
2021-02-24 04:19:11 +0800

    No one likes to read a long horizontal line of code. It's best practice split them.

---
+++ 3-code-quality/3-2-coding-style/3-2-1-syntax/3-2-1-2-line-length/line-length-0.js
@@ -0,0 +1,7 @@
+// backtick quotes ` allow to split the strig into multiple lines 
+let str = `
+  ECMA International's TC39 is a group of JavaScript developers, 
+  implementers, academics, and more, collaborating with teh community, 
+  to maintain and evolve the definition of JavaScript.
+`;
+
