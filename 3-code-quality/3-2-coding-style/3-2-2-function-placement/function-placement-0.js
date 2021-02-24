64fd95f20e27b09be309418bceb43595ce84e6aa
imamfarisi19 <edwardindo1@gmail.com>
2021-02-25 02:12:44 +0800

    declare the functions above the code that uses them

---
+++ 3-code-quality/3-2-coding-style/3-2-2-function-placement/function-placement-0.js
@@ -0,0 +1,18 @@
+// function declarations 
+function createElement() {
+  ...
+}
+
+function setHandler(elem) {
+  ...
+}
+
+function walkAround() {
+  ...
+}
+
+// the code which uses them 
+let elem = createElement(); 
+setHandler(elem)
+walkAround();
+
