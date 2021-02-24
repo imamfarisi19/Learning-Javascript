83740012ffa3dfb9f9b60748a9c0422aa5a27638
imamfarisi19 <edwardindo1@gmail.com>
2021-02-25 02:13:01 +0800

    code first, then functions

---
+++ 3-code-quality/3-2-coding-style/3-2-2-function-placement/function-placement-1.js
@@ -0,0 +1,18 @@
+// the code which uses the functions
+let elem = createElement();
+setHandler(elem);
+walkAround(); 
+
+// --- helper functions ---
+function createElement() {
+  ...
+}
+
+function setHandler(elem) {
+  ...
+}
+
+function walkAround() {
+  ...
+}
+
