b6c998953aaf8d0063699399a81c225c58eaddf6
imamfarisi19 <edwardindo1@gmail.com>
2021-02-25 22:54:08 +0800

    Then it might be a better variant to refactor it into functions

---
+++ 3-code-quality/3-3-comments/3-3-1-bad-comments/3-3-1-2-recipe-create-functions/create-functions-1.js
@@ -0,0 +1,17 @@
+addWhiskey(glass);
+addJuice(glass);
+
+function addWhiskey(container) {
+  for (let i = 0; i < 10; i++) {
+    let drop = getWhiskey();
+    //...
+  }
+}
+
+function addJuice(container) {
+  for (let t = 0; t < 3; t++){
+    let tomato = getTomato();
+    //...
+  }
+}
+
