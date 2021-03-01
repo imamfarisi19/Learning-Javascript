bee1f221312943946e3a433cc8edf679859646b0
imamfarisi19 <edwardindo1@gmail.com>
2021-03-02 01:16:54 +0800

    Here, the variable 'key' may be calculated at run-time or depend on the user input. And then we use it to access the property. That gives us a great deal of flexibility

---
+++ 4-objects-the-basics/4-2-square-brackets/square-brackets-3.js
@@ -0,0 +1,10 @@
+let user = {
+  name: "John",
+  age: 30
+};
+
+let key = prompt("What do you want to know aobut the user?", "name");
+
+// access by variable
+alert( user[key] ); // John (if emter "name")
+
