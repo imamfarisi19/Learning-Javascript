ed708edd4909eb736c7e2e6333e6ec1ffc953f0e
imamfarisi19 <edwardindo1@gmail.com>
2021-03-07 23:55:31 +0800

    So, to fix the issue with the phone codes, we can 'cheat' by making the codes non-integer. Adding a plus '+' sign before each code is enough

---
+++ 4-objects-the-basics/4-6-the-for-in-loop/4-6-1-ordered-like-an-object/ordered-like-an-object-3.js
@@ -0,0 +1,12 @@
+let codes = {
+  "+49": "Germany",
+  "+41": "Switzerland",
+  "+44": "Great Britain",
+  // ..,
+  "+1": "USA"
+};
+
+for (let code in codes) {
+  alert( +code ); // 49, 41, 44, 1
+}
+
