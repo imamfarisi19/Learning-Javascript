dc1c74f73db18a41bc0e9dbc6a10956bbd1e46a4
imamfarisi19 <edwardindo1@gmail.com>
2021-03-17 23:20:26 +0800

    If we have many lines of code all about creation of a single complex object, we can wrap them in constructor function, like this

---
+++ 4-objects-the-basics/4-5-constructor-operator-new/4-5-1-constructor-function/constructor-function-3.js
@@ -0,0 +1,8 @@
+let user = new function() {
+  this.name = "John";
+  this.isAdmin = false;
+
+  // ...other code for user creation
+  // maybe complex logic and statements
+  // local variables etc
+};
