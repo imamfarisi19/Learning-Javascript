f1ab610e640da5d9e793d5e8e7e69fdbf037f86b
imamfarisi19 <edwardindo1@gmail.com>
2021-03-19 05:20:42 +0800

    Is it possible to create functions [A] and [B] such as [new A() == new B()] /?

---
+++ 4-objects-the-basics/4-5-constructor-operator-new/4-5-5-tasks/two-functions-one-object.js
@@ -0,0 +1,18 @@
+function A() { ... }
+function B() { ... }
+
+let a = new A;
+let b = new B;
+
+alert (a == b); // true
+
+// --------------------------
+// answer
+
+let obj = {};
+
+function A() { return obj; }
+function B() { return obj; }
+
+alert( new A() == new B() ); // true
+
