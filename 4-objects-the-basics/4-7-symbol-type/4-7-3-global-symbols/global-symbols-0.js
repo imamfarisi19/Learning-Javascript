0a341648e55c065e090a818d339d15acc278e250
imamfarisi19 <edwardindo1@gmail.com>
2021-03-22 03:38:10 +0800

    that call checks the global registry, and if there's a symbol described as [key], then returns it, otherwise creates a new symbol [Symbol(key)] and stores in the registry by the given [key]

---
+++ 4-objects-the-basics/4-7-symbol-type/4-7-3-global-symbols/global-symbols-0.js
@@ -0,0 +1,9 @@
+// read from the global registry 
+let id = Symbol.for("id"); // if the symbol did not exist, it is created 
+
+// read it again (maybe from another part of the code)
+let idAgain = Symbol.for("id");
+
+// the same symbol
+alert(id == idAgain); // true 
+
