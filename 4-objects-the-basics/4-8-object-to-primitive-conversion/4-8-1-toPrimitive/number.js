bfcb2b4d6c1689347dba351ef0619fd175ce344d
imamfarisi19 <edwardindo1@gmail.com>
2021-03-23 08:22:53 +0800

    for an object-to-number conversion, like when we're doing maths

---
+++ 4-objects-the-basics/4-8-object-to-primitive-conversion/4-8-1-toPrimitive/number.js
@@ -0,0 +1,11 @@
+// explicit conversion 
+let num = Number(obj); 
+
+// maths (except binary plus)
+let n = +obj; // unary plus 
+let delta = date1 - date2;
+
+// less/greater comparison 
+let greater = user1 > user2; 
+
+
