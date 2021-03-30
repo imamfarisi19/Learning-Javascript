5358ad4a238e1361a51a7cd141f819e3a48c134e
imamfarisi19 <edwardindo1@gmail.com>
2021-03-30 10:01:45 +0800

    There are many correct solutions to the task. One of them is to adjust interval borders. To ensure the same intervals, we can generate values from [0.5 to 3.5], thus adding the required probabilites to the edges

---
+++ 5-data-types/5-2-number/5-2-8-tasks/a-random-integer-from-min-to-max/a-random-integer-from-min-to-max-1.js
@@ -0,0 +1,8 @@
+function randomIntege(min, max) {
+  // now rand is from (min-0.5) to (max+0.5)
+  let rand = min - 0.5 + Math.random() * (min - max + 1);
+  return Math.round(rand);
+}
+
+alert( randomInteger(1, 3) );
+
