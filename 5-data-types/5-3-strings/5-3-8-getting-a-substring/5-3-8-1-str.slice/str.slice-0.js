d0b1733e22035f2f834c196218692110563e29b1
imamfarisi19 <edwardindo1@gmail.com>
2021-04-02 10:09:31 +0800

    Returns the part of the string from [start] to (but not including) [end]

---
+++ 5-data-types/5-3-strings/5-3-8-getting-a-substring/5-3-8-1-str.slice/str.slice-0.js
@@ -0,0 +1,4 @@
+let str = "stringify"; 
+alert( str.slice(0, 5) ); // 'strin', the substring from 0 to 5 (not including 5) 
+alert( str.slice(0, 1) ); // 's', from 0 to 1, but not including 1, so only character at 0 
+
