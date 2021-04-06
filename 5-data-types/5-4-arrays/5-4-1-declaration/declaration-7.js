91127e655ffd93723aa2e2d20e0ed8df66876d78
imamfarisi19 <edwardindo1@gmail.com>
2021-04-06 21:23:21 +0800

    An array can store elements of any type

---
+++ 5-data-types/5-4-arrays/5-4-1-declaration/declaration-7.js
@@ -0,0 +1,9 @@
+// mix of values 
+let arr = [ 'Apple', {name: 'John'}, true, function() { alert('hello'); } ]; 
+
+// get the object at index 1 and then show its name 
+alert( arr[1]name ); // John 
+
+// get the function at index 3 and run it 
+arr[3](); // hello 
+
