82bc9f6edbb01cac77c5b0cccc8bf5b42ef2686c
imamfarisi19 <edwardindo1@gmail.com>
2021-04-08 12:28:44 +0700

    But they all break if we quit working with an array as with an "ordered collection" and start working with it as if it were a regular object

---
+++ 5-data-types/5-4-arrays/5-4-3-internals/internals-1.js
@@ -0,0 +1,6 @@
+let fruits = []; // make an array 
+
+fruits[99999] = 5; // assign a property with the index far greater than its length 
+
+fruits.age = 25; // create a property with an arbitrary name 
+
