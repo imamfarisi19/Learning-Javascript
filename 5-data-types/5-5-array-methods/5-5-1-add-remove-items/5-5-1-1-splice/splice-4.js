8262bad63111605c2eb23a4772e812fda6db0987
imamfarisi19 <edwardindo1@gmail.com>
2021-04-11 02:39:23 +0700

    The [splice] method is also able to insert the elements without any removals

---
+++ 5-data-types/5-5-array-methods/5-5-1-add-remove-items/5-5-1-1-splice/splice-4.js
@@ -0,0 +1,9 @@
+let arr = ["I", "Study", "JavaScript"]; 
+
+// from index 2
+// delete 0 
+// then insert "complex" and "languange" 
+arr.splice(2, 0, "complex", "languange"); 
+
+alert( arr ); // "I", "study", "complex", "languange", "JavaScript"
+
