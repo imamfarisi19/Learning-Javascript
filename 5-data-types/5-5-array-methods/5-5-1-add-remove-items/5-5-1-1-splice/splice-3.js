e34a9c43023db489221c37ccab40d28b5aa212f2
imamfarisi19 <edwardindo1@gmail.com>
2021-04-11 02:38:27 +0700

    Here we can see that [splice] returns the array of removed elements

---
+++ 5-data-types/5-5-array-methods/5-5-1-add-remove-items/5-5-1-1-splice/splice-3.js
@@ -0,0 +1,7 @@
+let arr = ["I", "study", "JavaScript", "right", "now"]; 
+
+// remove 2 first elements 
+let removed = arr.splice(0, 2);
+
+alert( removed ); // "I", "Study" <-- array of removed elements 
+
