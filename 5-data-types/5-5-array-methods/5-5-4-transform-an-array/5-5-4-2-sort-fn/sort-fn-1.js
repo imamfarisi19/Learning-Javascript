cce83ece4cc1f2309d07c4e4e6e2af2c1a458a52
imamfarisi19 <edwardindo1@gmail.com>
2021-04-13 01:40:16 +0700

    The function should compare two arbitrary values and return

---
+++ 5-data-types/5-5-array-methods/5-5-4-transform-an-array/5-5-4-2-sort-fn/sort-fn-1.js
@@ -0,0 +1,6 @@
+function compare(a, b) {
+  if (a > b) return 1; // if the first value is greater than the second 
+  if (a == b) return 0; // if values are equal 
+  if (a < b) return -1; // if the first value is less than the second 
+} 
+
