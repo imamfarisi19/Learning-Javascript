ba8385c4dbbdb59a0ad5df0d0f4078ae1975a2a0
imamfarisi19 <edwardindo1@gmail.com>
2021-04-16 20:44:01 +0700

    solution for map-to-objects-0.js

--- 5-data-types/5-5-array-methods/5-5-8-task/5-5-8-8-map-to-objects/map-to-objects-1.js
+++ 5-data-types/5-5-array-methods/5-5-8-task/5-5-8-8-map-to-objects/map-to-objects-1.js
@@ -1,10 +1,22 @@
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+let john = { name: "John", surname: "Smith", id: 1 }; 
+let pete = { name: "Pete", surname: "Hunt", id: 2 }; 
+let mary = { name: "Mary", surname: "Key", id: 3 }; 
 
-let users = { john, pete, mary }; 
+let users = [ john, pete, mary ]; 
 
-let names = users.map(item.name);
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+}));
 
-alert(names); // John, Pete, Mary 
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3}
+]
+*/
+
+alert( usersMapped[0].id ); // 1
+alert( usersMapped[0].fullName ); // John Smith 
 
