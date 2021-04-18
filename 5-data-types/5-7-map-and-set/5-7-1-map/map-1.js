cd27b0978d4ea250127514b9db4dabd788efa0cc
imamfarisi19 <edwardindo1@gmail.com>
2021-04-19 03:01:19 +0700

    Map can also use objects as keys

---
+++ 5-data-types/5-7-map-and-set/5-7-1-map/map-1.js
@@ -0,0 +1,10 @@
+let john = { name: "John" }; 
+
+// for every user, let's store their visits count 
+let visitsCountMap = new Map(); 
+
+// john is the key for the map 
+visitCountMap(jon, 123); 
+
+alert( visitCountMap.get(john) ); // 123 
+
