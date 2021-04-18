64c47e4225ba4eb75f9b83aad9883103b19d414e
imamfarisi19 <edwardindo1@gmail.com>
2021-04-19 03:16:13 +0700

    for looping over a [map], there are 3 methods

---
+++ 5-data-types/5-7-map-and-set/5-7-2-iteration-over-map/iteration-over-map-0.js
@@ -0,0 +1,21 @@
+let recipeMap = new Map([ 
+  ['cucumber', 500], 
+  ['tomatoes', 350], 
+  ['onion', 50] 
+]); 
+
+// iterates over keys (vegetables) 
+for (let vegetables of recipeMap.keys()) { 
+  alert(vegetables); // cucumber, tomatoes, onion 
+} 
+
+// iterate over values (amounts) 
+for (let amount of recipeMap.values()) {
+  alert(amount); // 500, 350, 50 
+} 
+
+// iterate over [key, value] entries
+for (let entry of recipeMap) { // the same as of recipeMap.entries()
+  alert(entry); // cucumber, 500 (and so on)
+}
+
