3fb67f2ffa0c638ccfdb988321773336d13f1b0a
imamfarisi19 <edwardindo1@gmail.com>
2021-04-20 20:20:50 +0700

    That's how the solution can look

---
+++ 5-data-types/5-8-tasks/5-8-2-filter-anagrams/filter-anagrams-2.js
@@ -0,0 +1,14 @@
+function aclean(arr) {
+  let obj = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let sorted = arr[i].toLowerCase().split("").obj[sorted] = arr[i];
+  }
+
+  return Object.values(obj);
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+alert( aclean(arr) ); 
+
