aebf38b073ea75e22127628c8cd7bb0596a796e1
imamfarisi19 <edwardindo1@gmail.com>
2021-04-21 18:06:05 +0700

    remove

--- 5-data-types/5-8-tasks/5-8-2-filter-anagrams/filter-anagrams.js
+++
@@ -1,4 +0,0 @@
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"]; 
-
-alert( aclean(arr) ); // "nap,teachers,ear" or "PAN,cheaters,era" 
-
--- 5-data-types/5-8-tasks/5-8-3-iterable-keys/iterable-keys-0.js
+++
@@ -1,9 +0,0 @@
-let map = new Map(); 
-
-map.set("name", "John"); 
-
-let keys = map.keys(); 
-
-// Error: keys.push is not a function 
-keys.push("more"); 
-
