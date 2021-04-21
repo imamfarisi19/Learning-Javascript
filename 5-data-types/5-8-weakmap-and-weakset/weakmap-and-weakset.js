e81205d46dace4d8c41688603fde839a6e55ceef
imamfarisi19 <edwardindo1@gmail.com>
2021-04-21 18:50:07 +0700

    As we know from the chapter Garbage collection, JavaScript engine keeps a value in memory while it is "reachable" and can potentially be used

---
+++ 5-data-types/5-8-weakmap-and-weakset/weakmap-and-weakset.js
@@ -0,0 +1,9 @@
+let john = { name: "John" }; 
+
+// the object can be accessed, john is the reference to it
+
+// overwrite the reference 
+john = null;
+
+// the object will be removed from memory 
+
