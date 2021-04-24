067977300650d9b3413a66a3d8e28b611f2f1c14
imamfarisi19 <edwardindo1@gmail.com>
2021-04-25 02:39:19 +0700

    Here's an example of how an array is destructured into variables

---
+++ 5-data-types/5-10-destructing-assignment/5-10-1-array-destructuring/array-destructuring-0.js
@@ -0,0 +1,11 @@
+// we have an array with the name and surname 
+let arr = ["John", "Smith"]; 
+
+// destructuring assignment 
+// sets firstName = arr[0] 
+// and surname = arr[1] 
+let [firstName, surname] = arr; 
+
+alert(firstName); // John 
+alert(surname); // Smith 
+
