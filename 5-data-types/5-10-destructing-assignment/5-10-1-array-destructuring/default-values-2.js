07506d20bb38736309ed769aebe1dab8b8cd0e42
imamfarisi19 <edwardindo1@gmail.com>
2021-04-25 03:43:38 +0700

    here we user the [prompt] function for two defaults

---
+++ 5-data-types/5-10-destructing-assignment/5-10-1-array-destructuring/default-values-2.js
@@ -0,0 +1,6 @@
+// runs only prompt for surname 
+let [name = prompt('name?'), surname = prompt('surname?')] = ["Julius"]; 
+
+alert(name); // Julius (from array)
+alert(surname); // Whatever prompt gets 
+
