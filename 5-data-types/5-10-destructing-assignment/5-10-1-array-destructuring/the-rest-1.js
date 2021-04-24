e14518a4603a2c8e5e3c67554a350851ba478c21
imamfarisi19 <edwardindo1@gmail.com>
2021-04-25 02:55:15 +0700

    If we'd like also to gather all that follows - we can add one more parameter that gets "the rest" using three dots ["..."].

---
+++ 5-data-types/5-10-destructing-assignment/5-10-1-array-destructuring/the-rest-1.js
@@ -0,0 +1,7 @@
+let [name1, name2, ...rest] = ["Julius", "Caesar", "Consul", "of the Roman Republic"]; 
+
+// rest is array of items, starting from the 3rd one 
+alert(rest[0]); // Consul 
+alert(rest[1]); // of the Roman Republic 
+alert(rest.length); // 2 
+
