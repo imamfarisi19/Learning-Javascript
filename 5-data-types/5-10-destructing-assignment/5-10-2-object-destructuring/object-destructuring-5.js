5eea94188159f6c105630f47bd07e7bc201b05b8
imamfarisi19 <edwardindo1@gmail.com>
2021-04-29 02:12:31 +0700

    in the code below [prompt] asks for [width], but not for [title]

---
+++ 5-data-types/5-10-destructing-assignment/5-10-2-object-destructuring/object-destructuring-5.js
@@ -0,0 +1,9 @@
+let options = {'
+  title: "Menu"
+}; 
+
+let {width = prompt("width?"), title = prompt("title?")} = options; 
+
+alert(title); // Menu 
+alert(width); // (Whatever the result of prompt is) 
+
