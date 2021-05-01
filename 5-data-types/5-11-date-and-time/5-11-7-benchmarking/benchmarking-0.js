9c25a065bd8777f9d8a9856cb7fa2d615cffe2e2
imamfarisi19 <edwardindo1@gmail.com>
2021-05-01 22:09:11 +0700

    Such performance measurements are often called 'benchmarks'

---
+++ 5-data-types/5-11-date-and-time/5-11-7-benchmarking/benchmarking-0.js
@@ -0,0 +1,10 @@
+// we have date1 and date2, which function faster returns their difference in ms?
+function diffSubtract(date1, date2) {
+  return date2 - date1;
+}
+
+// or
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime();
+}
+
