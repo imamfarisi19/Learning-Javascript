f08a0fd0ff2fb9bb4474f1ae1375a277aadd7f5e
imamfarisi19 <edwardindo1@gmail.com>
2021-05-01 22:10:15 +0700

    For more reliable benchmarking, the whole pack of benchmarks should be rerun multiple times

---
+++ 5-data-types/5-11-date-and-time/5-11-7-benchmarking/benchmarking-2.js
@@ -0,0 +1,29 @@
+function diffSubtract(date1, date2) {
+  return date2 - date1; 
+}
+
+function diffGetTime(date1, date2) {
+  return date2.getTime() - date1.getTime(); 
+} 
+
+function bench(f) {
+  let date1 = new Date(0):
+  let date2 = new Date(); 
+
+  let start = Date.now(); 
+  for (let i = 0; i < 100000; i++) f(date1, date2); 
+  return Date.now() - start;
+}
+
+let time1 = 0; 
+let time2 = 0;
+
+// run bench(diffSubtract) and bench(diffGetTime) each 10 times alternating 
+for (let i = 0; i < 10; i++) {
+  time1 += bench(diffSubtract);
+  time2 += bench(diffGetTime); 
+}
+
+alert( 'Total time for diffSubtract: ' + time1 ); 
+alert( 'Total time for diffGetTime: ' + time2 ); 
+
