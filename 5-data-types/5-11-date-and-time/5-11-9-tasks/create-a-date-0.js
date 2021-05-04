33893665f1fa7397492a34b5d8f90ed392b0665d
imamfarisi19 <edwardindo1@gmail.com>
2021-05-05 01:41:43 +0700

    remove

--- 5-data-types/5-11-date-and-time/5-11-9-tasks/create-a-date-0.js
+++
@@ -1,4 +0,0 @@
-// new Date(year, month, date, hour, minute, second, milisecond)
-let d1 = new Date(2012, 1, 20, 3, 12);
-alert(d1); 
-
--- 5-data-types/5-11-date-and-time/5-11-9-tasks/create-a-date-1.js
+++
@@ -1,4 +0,0 @@
-// new Date(datastring); 
-let d2 = new Date("February 20, 2012, 3:12:00"); 
-alert(d2); 
-
