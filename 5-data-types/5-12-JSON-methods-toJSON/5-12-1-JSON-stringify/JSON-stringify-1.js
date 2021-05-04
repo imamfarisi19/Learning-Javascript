e7d58e5e0bbe1adaa5a8fda54fc631143d2e0a00
imamfarisi19 <edwardindo1@gmail.com>
2021-05-05 02:04:50 +0700

    [JSON.stringify] can be applied to primitives as well.

---
+++ 5-data-types/5-12-JSON-methods-toJSON/5-12-1-JSON-stringify/JSON-stringify-1.js
@@ -0,0 +1,10 @@
+// a number in JSON is just a number 
+alert( JSON.stringify(1) ); // 1 
+ 
+// a string in JSON is still a string, but double-quoted 
+alert( JSON.stringify('test') ); // "test" 
+
+alert( JSON.stringify(true) ); // true 
+
+alert( JSON.stringify([1, 2, 3]) ); // [1, 2, 3]
+
