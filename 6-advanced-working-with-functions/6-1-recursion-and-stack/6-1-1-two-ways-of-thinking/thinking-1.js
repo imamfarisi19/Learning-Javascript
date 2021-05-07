8eb0c4fedbd07fb25aa6215e82c590287385368b
imamfarisi19 <edwardindo1@gmail.com>
2021-05-08 00:26:53 +0700

    Iterative thinking: the [for] loop

---
+++ 6-advanced-working-with-functions/6-1-recursion-and-stack/6-1-1-two-ways-of-thinking/thinking-1.js
@@ -0,0 +1,13 @@
+function pow(x, n) {
+  let result = 1;
+
+  // multiply result by x n times in the loop
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+alert( pow(2, 3) ); // 8
+
