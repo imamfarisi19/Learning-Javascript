42abca4ab30760fdf4c37710e07c7453fe4d5fec
imamfarisi19 <edwardindo1@gmail.com>
2021-05-07 23:14:59 +0700

    Let's pass to [JSON.parse] the reviving function as the second argument, that returns all values "as is", but [date] will become a [Date]

---
+++ 5-data-types/5-12-JSON-methods-toJSON/5-12-6-using-reviver/using-reviver-2.js
@@ -0,0 +1,9 @@
+let str = '{"title":"Conference","date":"2017-11-30T12:00:00.000Z"}';
+
+let meetup = JSON.parse(str, function(key, value) {
+  if (key == 'date') return new Date(value);
+  return value;
+});
+
+alert( meetup.date.getDate() ); // now works! 
+
