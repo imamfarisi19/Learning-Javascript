671be681dfc6dc0a568a2e6311ede93b56669e5a
imamfarisi19 <edwardindo1@gmail.com>
2021-05-07 23:26:40 +0700

    Write [replacer] function to stringify everything, but remove properties that reference [meetup]

---
+++ 5-data-types/5-12-JSON-methods-toJSON/5-12-7-tasks/exclude-backreferences-0.js
@@ -0,0 +1,26 @@
+let room = {
+  number: 23
+}; 
+
+let meetup = { 
+  title: "Conference", 
+  occupiedBy: [{name: "John"}, {name: "Alice"}], 
+  place: room 
+}; 
+
+// circular referenes 
+room.occupiedBy = meetup;
+meetup.self = meetup; 
+
+alert( JSON.stringify(meetup, function replacer(key, value) {
+  return (key != "" && value == meetup) ? undefined : value;
+}));
+
+/* result should be:
+{
+  "title": "Conference",
+  "occupiedBy":[{"name":"John"},{"name":"Alice"}],
+  "place":{"number":23}
+}
+*/
+
