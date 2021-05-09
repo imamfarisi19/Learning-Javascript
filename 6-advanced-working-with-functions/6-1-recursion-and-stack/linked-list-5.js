87f2d4c1493ae14baa5bd8f7610de29a643dbc27
imamfarisi19 <edwardindo1@gmail.com>
2021-05-09 21:37:43 +0700

    and surely we can insert or remove items in any place

---
+++ 6-advanced-working-with-functions/6-1-recursion-and-stack/linked-list-5.js
@@ -0,0 +1,8 @@
+let list = {value: 1}; 
+list.next = {value: 2}; 
+list.next.next = {value: 3}; 
+list.next.next.next = {value: 4}; 
+
+// prepend the new value to the list 
+list = {value: "new item", next: list}; 
+
