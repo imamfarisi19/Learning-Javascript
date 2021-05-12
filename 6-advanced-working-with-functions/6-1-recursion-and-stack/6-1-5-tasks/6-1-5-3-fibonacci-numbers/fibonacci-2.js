553021d304387dc31ec5dd9e51f6b326a0867adb
imamfarisi19 <edwardindo1@gmail.com>
2021-05-13 00:23:03 +0700

    Here are the steps of the new algorithm in details

---
+++ 6-advanced-working-with-functions/6-1-recursion-and-stack/6-1-5-tasks/6-1-5-3-fibonacci-numbers/fibonacci-2.js
@@ -0,0 +1,11 @@
+// a = fib(1), b = fib(2), these values are by definition 1
+let a = 1, b = 1;
+
+// get c = fib(3) as their sum
+let c = a + b;
+
+/* we now have fib(1), fib(2), fib(3)
+a  b  c
+1, 1, 2
+*/
+
