a438e538548136e8dfcc7255a236545c759f11ce
imamfarisi19 <edwardindo1@gmail.com>
2021-05-16 22:37:10 +0700

    Please note that if [f()] is called many times, and resulting functions are saved, then all corresponding Lexical Environment objects will also be retained in memory.

---
+++ 6-advanced-working-with-functions/6-3-variable-scope-closure/6-3-4-garbage-collection/garbage-collection-1.js
@@ -0,0 +1,10 @@
+function f() {
+  let value = Math.random();
+
+  return function() { alert(value); };
+}
+
+// 3 functions in array, every one of them links to Lexical Environment
+// from the corresponding f() run
+let arr = [f(), f(), f()]; 
+
