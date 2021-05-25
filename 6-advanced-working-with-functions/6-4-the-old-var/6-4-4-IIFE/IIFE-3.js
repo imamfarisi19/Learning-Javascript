1b72c35645ab673805da85c13078822887bebe48
imamfarisi19 <edwardindo1@gmail.com>
2021-05-25 14:38:12 +0700

    There exist other ways besides parentheses to tell JavaScript that we mean a Function Expression

---
+++ 6-advanced-working-with-functions/6-4-the-old-var/6-4-4-IIFE/IIFE-3.js
@@ -0,0 +1,17 @@
+// Ways to create IIFE 
+(function() {
+	alert("Parentheses around the function"); 
+})(); 
+
+(function(){ 
+  alert("Parentheses around the whole thing"); 
+}()); 
+
+!function() { 
+	alert("Bitwise NOT operator starts the expression"); 
+}(); 
+
++function() {
+	alert("Unary plus starts the expression");
+}(); 
+
