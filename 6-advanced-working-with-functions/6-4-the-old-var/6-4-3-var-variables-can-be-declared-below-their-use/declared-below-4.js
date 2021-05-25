d6dd5bc6d5492867af75e90045bcd7379322cdaf
imamfarisi19 <edwardindo1@gmail.com>
2021-05-25 14:22:03 +0700

    So the code works essentially like this

---
+++ 6-advanced-working-with-functions/6-4-the-old-var/6-4-3-var-variables-can-be-declared-below-their-use/declared-below-4.js
@@ -0,0 +1,10 @@
+function sayHi() {
+	var phrase; // declaration works at the start...
+
+	alert(phrase); // undefined
+
+	phrase = "Hello"; // ...assignment - when the execution reaches it.
+}
+
+sayHi();
+
