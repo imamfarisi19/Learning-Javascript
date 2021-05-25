19b95a2340559f2f71426e4d112f9b273bd3074c
imamfarisi19 <edwardindo1@gmail.com>
2021-05-25 10:41:09 +0700

    we make it even verbose, like this

---
+++ 6-advanced-working-with-functions/6-3-variable-scope-closure/6-3-5-tasks/6-3-5-9-sort-by-field/sort-0.js
@@ -0,0 +1,16 @@
+let users = [
+	{ name: "John", age: 20, surname: "Johnson" },
+	{ name: "Pete", age: 18, surname: "Peterson" },
+	{ name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+// by name (Ann, John, Pete)
+users.sort((a, b) => a.name > b.name ? 1 : -1);
+
+// by age (Pete, Ann, John) 
+users.sort((a, b) => a.age > b.age ? 1 : -1); 
+
+function byField(fieldName) {
+	return (a, b) =>a[fieldName] > b[fieldName] ? 1 : -1;
+}
+
