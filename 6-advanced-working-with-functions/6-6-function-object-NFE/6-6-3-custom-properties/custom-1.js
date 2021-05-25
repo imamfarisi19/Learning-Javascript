7bf3bef431fe0d802e2411ed52d701dbc86171d9
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 06:08:21 +0700

    Function properties can replace closures sometimes.

---
+++ 6-advanced-working-with-functions/6-6-function-object-NFE/6-6-3-custom-properties/custom-1.js
@@ -0,0 +1,17 @@
+function makeCounter() {
+	// instead off:
+	// let count = 0
+	
+	function counter() {
+		return counter.count++;
+	};
+
+	counter.count = 0;
+
+	return counter;
+}
+
+let counter = makeCounter(); 
+alert( counter() ); // 0 
+alert( counter() ); // 1 
+
