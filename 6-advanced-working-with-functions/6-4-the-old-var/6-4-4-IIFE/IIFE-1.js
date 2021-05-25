a2db2fee87bc411549c7dbcfe3edbacd7bcde4f2
imamfarisi19 <edwardindo1@gmail.com>
2021-05-25 14:34:04 +0700

    The function Expression is wrapped with parenthesis (function{...}), because when Javascript engine encounters "function" in the main code, it understands it as the start of a function declaration.

---
+++ 6-advanced-working-with-functions/6-4-the-old-var/6-4-4-IIFE/IIFE-1.js
@@ -0,0 +1,8 @@
+// Tries to declare and immediately call a function
+function() { // <-- SyntaxError: Function statements require a function name 
+
+	var message = "Hello";
+
+	alert(message); // Hello
+}();
+
