37c4e7d378a38694c2a53b29b894162a7d464b42
imamfarisi19 <edwardindo1@gmail.com>
2021-05-25 15:21:43 +0700

    If a value is so important that you'd like to make it available globally, write it directly as a property

---
+++ 6-advanced-working-with-functions/6-5-global-object/6-5-0-global-object/window-3.js
@@ -0,0 +1,12 @@
+// make current user information global, to let all scripts access 
+window.currentUser = {
+	name: "John"
+}; 
+
+// somewhere else in code 
+alert(currentUser.name); // John 
+
+// or, if we have a local variable with the name "currentUser"
+// get it from window explicitly (safe!)
+alert(window.currentUser.name); // John 
+
