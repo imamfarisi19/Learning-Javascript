07fb1a91db8ba5c44dfa3b0adb43dc0ede4188c4
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 06:34:24 +0700

    For instane, the function [sayHi] below calls itself again with ["Guest"] if no [who] is provided

---
+++ 6-advanced-working-with-functions/6-6-function-object-NFE/6-6-4-named-function-expression/named-function-3.js
@@ -0,0 +1,13 @@
+let sayHi = function func(who) {
+	if(who) {
+		alert(`Hello, ${who}`);
+	} else {
+		func("Guest"); // use func to re-call itself
+	}
+}; 
+
+sayHi(); // Hello, Guest 
+
+// But this won't work: 
+func(); // Error, func is not defined (not visible outside of the function)
+
