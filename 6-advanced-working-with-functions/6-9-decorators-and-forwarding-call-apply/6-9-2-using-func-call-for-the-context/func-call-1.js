5625caf7a6c91530f39a06e117b8933cc1a2c992
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 15:57:05 +0700

    [sayHi.call(user)] runs [sayHi] providing [this=user], and the next line sets [this=admin]

---
+++ 6-advanced-working-with-functions/6-9-decorators-and-forwarding-call-apply/6-9-2-using-func-call-for-the-context/func-call-1.js
@@ -0,0 +1,11 @@
+function sayHi() { 
+	alert(this.name); 
+} 
+
+let user = { name: "John" }; 
+let admin = { name: "Admin" }; 
+
+// use call to pass different objects as "this" 
+sayHi.call( user ); // John 
+sayHi.call( admin ); // Admin 
+
