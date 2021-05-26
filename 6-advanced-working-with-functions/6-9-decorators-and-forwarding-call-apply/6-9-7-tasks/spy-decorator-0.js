0529ed379e7e67b20d6c25b8b157ecad39ac9def
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 17:41:46 +0700

    every call is saved as an array arguments

---
+++ 6-advanced-working-with-functions/6-9-decorators-and-forwarding-call-apply/6-9-7-tasks/spy-decorator-0.js
@@ -0,0 +1,13 @@
+function work(a, b) {
+	alert( a + b ); // work is an arbitrary function or method
+}
+
+work = spy(work);
+
+work(1, 2); // 3 
+work(4, 5); // 9 
+
+for (let args of work.calls) {
+	alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+}
+
