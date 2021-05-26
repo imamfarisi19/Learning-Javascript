874bb8f0b072203b26ba622180c5fd728f22b054
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 17:21:26 +0700

    Create a decorator delay

---
+++ 6-advanced-working-with-functions/6-9-decorators-and-forwarding-call-apply/6-9-7-tasks/delaying-0.js
@@ -0,0 +1,11 @@
+function f(x) {
+	alert(x);
+}
+
+// create wrappers 
+let f1000 = delay(f, 1000); 
+let f1500 = delay(f, 1500); 
+
+f1000("test"); // shows "test" after 1000ms 
+f1500("test"); // shows "test" after 1500ms 
+
