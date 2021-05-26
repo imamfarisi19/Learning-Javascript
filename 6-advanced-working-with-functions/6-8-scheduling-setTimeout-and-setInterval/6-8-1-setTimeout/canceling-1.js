7e4d63a4ca9323b602d27c166c5354a474899f9f
imamfarisi19 <edwardindo1@gmail.com>
2021-05-26 11:12:02 +0700

    In the code below, we schedule the function and then cancel it (changed out mind). As a result, nothing happens

---
+++ 6-advanced-working-with-functions/6-8-scheduling-setTimeout-and-setInterval/6-8-1-setTimeout/canceling-1.js
@@ -0,0 +1,6 @@
+let timerId = setTimeout(() => alert("never happens"), 1000); 
+alert(timerId); // timer identifier 
+
+clearTimeout(timerId);
+alert(timerId); // same identifier (doesn't become null after canceling)
+
