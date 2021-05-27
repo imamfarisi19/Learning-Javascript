e6e5fc2212e4cae1f3384dd086b870e40b09c613
imamfarisi19 <edwardindo1@gmail.com>
2021-05-27 10:22:43 +0700

    All arguments are passed to the original [func]

---
+++ 6-advanced-working-with-functions/6-10-function-binding/6-10-3-solution-2-bind/bind-2.js
@@ -0,0 +1,13 @@
+let user = { 
+	firstName = "John" 
+}; 
+
+function func(phrase) { 
+	alert(phrase + ', ' + this.firstName); 
+} 
+
+// bind this to user 
+let funcUser = func.bind(user); 
+
+funcUser("Hello"); // Hello, John (argument "Hello" is passed, and this=user) 
+
