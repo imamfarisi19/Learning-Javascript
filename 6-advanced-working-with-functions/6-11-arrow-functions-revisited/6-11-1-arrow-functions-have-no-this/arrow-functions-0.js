379e31e8e863819069ee85bc72bdc834fc3d43ae
imamfarisi19 <edwardindo1@gmail.com>
2021-05-27 13:14:39 +0700

    For instance, we can use it to iterate inside an object method

---
+++ 6-advanced-working-with-functions/6-11-arrow-functions-revisited/6-11-1-arrow-functions-have-no-this/arrow-functions-0.js
@@ -0,0 +1,13 @@
+let group = {
+	title: "Our Group";
+	students: ["John", "Pete", "Alice"],
+
+	showList() {
+		this.students.forEach(
+			student => alert(this.title + ': ' + student);
+		);
+	}
+};
+
+group.showList();
+
