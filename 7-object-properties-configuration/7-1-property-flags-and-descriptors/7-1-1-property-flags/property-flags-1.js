342af5fbd4fe383a12351c0c0fcab06c6564f052
imamfarisi19 <edwardindo1@gmail.com>
2021-05-28 17:59:58 +0700

    for instance, here a property [name] is created with all falsy flags

---
+++ 7-object-properties-configuration/7-1-property-flags-and-descriptors/7-1-1-property-flags/property-flags-1.js
@@ -0,0 +1,18 @@
+let user = {}; 
+
+Object.defineProperty(user, "name", { 
+	value: "John" 
+}); 
+
+let descriptor = Object.getOwnPropertyDescriptor(user, 'name'); 
+
+alert( JSON.stringify(descriptor, null, 2 ) ); 
+/*
+{
+	"value": "John",
+	"writable": "false",
+	"enumerable": "false", 
+	"configurable": "false" 
+} 
+*/ 
+
