d7756ba2b7f861915058a752c98efb97320b748c
imamfarisi19 <edwardindo1@gmail.com>
2021-06-04 17:08:31 +0700

    The for..in loop iterates over inherited properties too

---
+++ 8-prototypes-inheritance/8-1-prototypal-inheritance/8-1-4-for-in-loop/for-in-0.js
@@ -0,0 +1,15 @@
+let animal = {
+	eats: true
+}; 
+
+let rabbit = {
+	jumps: true, 
+	__proto__: animal
+}; 
+
+// Object.keys only returns own keys 
+alert(Object.keys(rabbit)); // jumps 
+
+// for..in loops over both own and inherited keys 
+for(let prop in rabbit) alert(prop); // jumps, then eats 
+
