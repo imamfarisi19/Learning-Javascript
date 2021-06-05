a9a084b64ed1f372feeb58daf10a98cf425d6c0f
imamfarisi19 <edwardindo1@gmail.com>
2021-06-05 23:46:17 +0700

    Or, alternatively recreate the constructor property manually

---
+++ 8-prototypes-inheritance/8-2-f-prototype/f-prototype-6.js
@@ -0,0 +1,7 @@
+function Rabbit() {}
+
+// Not overwrite Rabbit.prototypes totally 
+// just add to it
+Rabbit.prototype.jumps = true
+// the default Rabbit.ptotoype.constructor is preserved 
+
---
+++ 8-prototypes-inheritance/8-2-f-prototype/f-prototype-7.js
@@ -0,0 +1,7 @@
+Rabbit.prototype = {
+	jumps: true,
+	constructor: Rabbit
+}; 
+
+// now constructor is also correct, because we added it 
+
