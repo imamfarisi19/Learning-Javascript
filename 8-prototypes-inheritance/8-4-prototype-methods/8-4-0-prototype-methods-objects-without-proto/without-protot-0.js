bee8b166440b4d291f54a39b5e39c7aafd59563e
imamfarisi19 <edwardindo1@gmail.com>
2021-06-06 14:40:20 +0700

    These should be used instead of __proto__

---
+++ 8-prototypes-inheritance/8-4-prototype-methods/8-4-0-prototype-methods-objects-without-proto/without-protot-0.js
@@ -0,0 +1,13 @@
+let animal = {
+	eats: true
+}; 
+
+// create a new object with animal as a prototype 
+let rabbit = Object.create(animal); 
+
+alert(rabbit.eats); // true 
+
+alert(Object.getPrototypeOf(rabbit) === animal); // true 
+
+Object.setPrototypeOf(rabbit, {}); // change the prototype of rabbit to {}
+
