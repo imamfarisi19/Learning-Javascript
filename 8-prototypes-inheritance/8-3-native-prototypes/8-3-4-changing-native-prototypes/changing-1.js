b791372ee5788716bdb01cb8da8b5a6beb860c02
imamfarisi19 <edwardindo1@gmail.com>
2021-06-06 13:37:41 +0700

    We may then implement it manually and populate the built-in prototype with it

---
+++ 8-prototypes-inheritance/8-3-native-prototypes/8-3-4-changing-native-prototypes/changing-1.js
@@ -0,0 +1,15 @@
+if (!String.prototype.repeat) { // if there's no such method 
+	// add it to the prototype
+	
+	String.prototype.repeat = function(n) {
+		// repeat the string n times
+		
+		// actually, the code should be a little bit more complex than that 
+		// (the full algorithm is in the specification) 
+		// but even an imperfect polyfill is often considered good enough 
+		return new Array(n + 1).join(this);
+	};
+}
+
+alert( "La".repeat(3) ); // LaLaLa 
+
