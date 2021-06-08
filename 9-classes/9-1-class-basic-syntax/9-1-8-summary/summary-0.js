883060631d946d056d21d1211944d94d0729b309
imamfarisi19 <edwardindo1@gmail.com>
2021-06-08 17:38:57 +0700

    The basic class syntax looks like this

---
+++ 9-classes/9-1-class-basic-syntax/9-1-8-summary/summary-0.js
@@ -0,0 +1,17 @@
+class MyClass {
+	prop = value; // property 
+
+	constructor(...) { // constructor 
+		// ...
+	}
+
+	method(...) {} // method 
+
+	get something(...) {} // getter method 
+	set something(...) {} // setter method 
+
+	[Symbol.iterator]() {} // method with computed name (symbol here) 
+	// ...
+}
+
+
