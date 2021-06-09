d275e55b61cae05ac0d54ad27ab8fefe7e7440c9
imamfarisi19 <edwardindo1@gmail.com>
2021-06-09 16:14:19 +0700

    So here's the fix

---
+++ 9-classes/9-3-static-properties-and-methods/9-3-4-tasks/class-extends-object-2.js
@@ -0,0 +1,11 @@
+class Rabbit extends Object {
+	constructor(name) {
+		super(); // need to call the parent constructor when inheriting 
+		this.name = name;
+	}
+}
+
+let rabbit = new Rabbit("Rab"); 
+
+alert( rabbit.hasOwnProperty('name') ); // true 
+
