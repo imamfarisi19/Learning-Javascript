3afbb0f9b647521a97c517a0b2e5e5cb9e5e637c
imamfarisi19 <edwardindo1@gmail.com>
2021-06-10 14:40:01 +0700

    in other words, compare one after another

---
+++ 9-classes/9-6-class-checking-instanceof/9-6-1-the-instanceof-operator/instanceof-4.js
@@ -0,0 +1,7 @@
+obj.__proto__ === Class.prototype? 
+obj.__proto__.__proto__ === Class.prototype? 
+obj.__proto__.__ptoto__.__proto__ === Class.prototype 
+...
+// if any answer is true, return true
+// otherwise, if we reached the end of the chain, return false 
+
