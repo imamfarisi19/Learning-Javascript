3ad70124b1cf4dfab56c8566e28b7183a153d103
imamfarisi19 <edwardindo1@gmail.com>
2021-06-22 00:33:23 +0700

    The natural solution would be to put the second loadScript call inside the callback, like this

---
+++ 11-promises-async-await/11-1-callback-in-callback/callback-0.js
@@ -0,0 +1,10 @@
+loadScript('/my/script.js', function(script) { 
+	
+	alert(`Cool, the ${script.src} is loaded, let's load one more`); 
+
+	loadScript('/my/script2.js', function(script) {
+		alert(`Cool, the second script is loaded`); 
+	});
+
+});
+
