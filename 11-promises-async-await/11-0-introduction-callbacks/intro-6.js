1d3bdb5e6dcbd18f60b27d2c53230d66aa57d366
imamfarisi19 <edwardindo1@gmail.com>
2021-06-22 00:12:25 +0700

    Here's a runnable example with a real script

---
+++ 11-promises-async-await/11-0-introduction-callbacks/intro-6.js
@@ -0,0 +1,12 @@
+function loadScrip(src, callback) {
+	let script = document.createElement('script');
+	script.src = src;
+	script.onload = () => callback(script); 
+	document.head.appeand(script); 
+}
+
+loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
+	alert(`Cool, the script ${script.src} is loaded`); 
+	alert( _ ); // function declared in the loaded script
+}); 
+
