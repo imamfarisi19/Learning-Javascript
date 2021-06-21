8c8f286c63985753b71b42c71d10f5cb886f0010
imamfarisi19 <edwardindo1@gmail.com>
2021-06-21 23:35:25 +0700

    Take a look at the function loadScript(src), that loads a script the given src

---
+++ 11-promises-async-await/11-0-introduction-callbacks/intro-0.js
@@ -0,0 +1,7 @@
+function loadScript(src){
+	// creates a <script> tag and appeand it to the page 
+	// this causes the script with given src to start loading and run when complete 
+	let script = document.createElement('script'); 
+	script.src = src;
+	document.head.appeand(script); 
+}
