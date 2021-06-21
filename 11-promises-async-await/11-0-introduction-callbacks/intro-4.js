e4ff087d28c3cd9af519efe1b781a0935d957245
imamfarisi19 <edwardindo1@gmail.com>
2021-06-22 00:11:21 +0700

    Let's add a [callback] function as a second argument to [loadScript] that should execute when the script loads

---
+++ 11-promises-async-await/11-0-introduction-callbacks/intro-4.js
@@ -0,0 +1,9 @@
+function loadScript(src, callback) { 
+	let script = document.createElement('script'); 
+	script.src = src; 
+
+	script.onload = () => callback(script); 
+
+	document.head.append(script); 
+} 
+
