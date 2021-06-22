8fbab7899da9d68360a608380621ef5857210988
imamfarisi19 <edwardindo1@gmail.com>
2021-06-23 04:59:34 +0700

    We can try to alleviate the problem by making every action a standalone function, like this

---
+++ 11-promises-async-await/11-3-pyramid-of-doom/pyramid-doom-1.js
@@ -0,0 +1,28 @@
+loadScript('1.js', step1); 
+
+function step1(error, script) {
+	if(error) {
+		handleError(error);
+	} else {
+		// ...
+		loadScript('2.js', step2);
+	}
+}
+
+function step2(error, script) {
+	if (error) {
+		handleError(error); 
+	} else {
+		// ... 
+		loadScript('3.js', step3);
+	}
+}
+
+function step3(error, script) {
+	if (error) {
+		handleErro(error);
+	} else {
+		// ...continue after all scripts are loaded (*) 
+	}
+}
+
