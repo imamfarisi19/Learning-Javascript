237a5e1e8c26d874a303d432af65c76a8d8ee091
imamfarisi19 <edwardindo1@gmail.com>
2021-06-23 05:29:37 +0700

    and now an example of the executor rejecting the promise with an error

---
+++ 11-promises-async-await/11-2-promise/11-2-0-promise/promise-2.js
@@ -0,0 +1,5 @@
+let promise = new Promise(function(resolve, reject) {
+	// after 1 second signal that the job is finished with an error 
+	setTimeout(() => reject(new Error("Whoops!")), 1000); 
+}); 
+
