5a8dab97b3e97851d15da49485bd321f52c59319
imamfarisi19 <edwardindo1@gmail.com>
2021-06-23 04:58:58 +0700

    But for multiple asynchronous actions that follow one after another we'll have code like this

---
+++ 11-promises-async-await/11-3-pyramid-of-doom/pyramid-doom-0.js
@@ -0,0 +1,22 @@
+loadScript('1.js', function(error, script) {
+	if (error) {
+		handleError(error); 
+	} else {
+		// ... 
+		loadScript('2.js', function(error, script) {
+			if (error) {
+				handleError(error);
+			} else {
+				// ... 
+				loadScript('3.js', function(error, script) {
+					if (error) {
+						handleError(error);
+					} else {
+						// ...continue after all scripts are loaded (*) 
+					}
+				});
+			}
+		});
+	}
+});
+
