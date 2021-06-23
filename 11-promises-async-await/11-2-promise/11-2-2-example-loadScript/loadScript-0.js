ef75d4979c619c24ec0bd1d5bd2d21b5860b4246
imamfarisi19 <edwardindo1@gmail.com>
2021-06-24 01:47:39 +0700

    Here's the callback-based variant, just to remind us of it

---
+++ 11-promises-async-await/11-2-promise/11-2-2-example-loadScript/loadScript-0.js
@@ -0,0 +1,10 @@
+function loadScript(src, callback) {
+	let script = document.createElement('script'); 
+	script.src = src; 
+
+	script.onload = () => callback(null, script);
+	script.onerror = () => callback(new Error(`Script load error for ${src}`)); 
+
+	document.head.appeand(script);
+}
+
