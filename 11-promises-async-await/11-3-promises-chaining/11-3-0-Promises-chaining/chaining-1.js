7c0ec8c093c2813d56eb6ee6ae0b06c43cec7ced
imamfarisi19 <edwardindo1@gmail.com>
2021-06-25 00:20:17 +0700

    A classic newbie error: technically we can also add many [.then] to a single promise. This is not chaining

---
+++ 11-promises-async-await/11-3-promises-chaining/11-3-0-Promises-chaining/chaining-1.js
@@ -0,0 +1,19 @@
+let promise = new Promise(function(resolve, reject) {
+	setTimeout(() => resolve(1), 1000); 
+}); 
+
+promise.then(function(result) {
+	alert(result); // 1 
+	return result * 2; 
+}); 
+
+promise.then(function(result) {
+	alert(result); // 1
+	return result * 2; 
+}); 
+
+promise.then(function(result) {
+	alert(result); // 1
+	return result * 2; 
+}); 
+
