8550d24f3f3f7d747565c203ff2aca4ec222e963
imamfarisi19 <edwardindo1@gmail.com>
2021-06-28 05:42:18 +0700

    For instance, if we have an array of URLs, we can fetch them all like this

---
+++ 11-promises-async-await/11-5-promise-api/11-5-1-promise-all/promise-all-2.js
@@ -0,0 +1,15 @@
+let urls = [
+	'https://api.github.com/users/iliakan',
+	'https://api.github.com/users/remy', 
+	'https://api.github.com/users/jeresig'
+]; 
+
+// map every url to the promise of the fetch 
+let requests = urls.map(url => fetch(url)); 
+
+// Promise.all waits until all jobs are resolved 
+Promise.all(requests) 
+	.then(responses => responses.forEach(
+		response => alert(`${response.url}: ${response.status}`) 
+	)); 
+
