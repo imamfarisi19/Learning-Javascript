5bd7be8f7df5af18b2bed12dbeb57cfd2f82b21e
imamfarisi19 <edwardindo1@gmail.com>
2021-07-06 02:45:25 +0800

    Here's an example when all promises fail

---
+++ 11-promises-async-await/11-5-promise-api/11-5-4-promise-any/promise-any-2.js
@@ -0,0 +1,8 @@
+Promise.any([
+	new Promise((resolve, reject) => setTimeout(() => reject(new Error("Ouch!")), 1000)),
+	new Promise((resolve, reject) => setTimeout(() => reject(new Error("Error!")), 2000))
+]).catch(error => {
+	console.log(error.errors[0]); // Error: Ouch!
+	console.log(error.errors[1]); // Error: Error 
+}); 
+
