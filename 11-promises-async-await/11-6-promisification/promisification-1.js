adc9e3f2beaa80546a289dd843f94c7fc18fd7bc
imamfarisi19 <edwardindo1@gmail.com>
2021-07-06 23:51:52 +0800

    In ohter words, we pass it only [src] (no [callback]) and get a promise in return

--- 11-promises-async-await/11-6-promisification/promisification-1.js
+++ 11-promises-async-await/11-6-promisification/promisification-1.js
@@ -1,22 +1,12 @@
-function promisify(f) {
-	return function (...args) { // return a wrapper-function (*)
-		return new Promise((resolve, reject) => {
-			function callback(err, result) { // our custom callback for f (**) 
-				if (err) {
-					reject(err);
-				} else {
-					resolve(result);
-				}
-			}
-
-			args.push(callback); // append our custom callback to the end of f arguments 
-
-			f.call (this, ...args); // call the original function
+let loadScriptPromise = function(src) { 
+	return new Promise((resolve, reject) => { 
+		loadScript(src, (err, script) => { 
+			if (err) reject(err); 
+			else resolve(script); 
 		});
-	};
-}
+	});
+};
 
 // usage: 
-let loadScriptPromise = promisify(loadScript);
-loadScriptPromise(...).then(...); 
+// loadScriptPromise('path/script.js').then(...) 
 
