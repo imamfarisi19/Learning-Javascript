56552fabecd0841f693ecbc6776ca3163cf8adea
imamfarisi19 <edwardindo1@gmail.com>
2021-07-09 16:51:28 +0800

    For instance, here we creaet the generator and get its first yielded value

---
+++ 12-generators-advanced-iteration/12-1-generators/12-1-1-generator-functions/generator-functions-2.js
@@ -0,0 +1,20 @@
+function* generateSequence() {
+	yield 1;
+	yield 2;
+	return 3; 
+}
+
+let generator = generateSequence();
+
+let one = generator.next(); 
+
+alert(JSON.stringify(one)); // {value: 1, done: false} 
+
+let two = generator.next(); 
+
+alert(JSON.stringify(two)); // {value: 2, done: false} 
+
+let three = generator.next(); 
+
+alert(JSON.stringify.next(three)); // {value: 3, done: true} 
+
