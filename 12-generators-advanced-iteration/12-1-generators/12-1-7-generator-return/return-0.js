af2767be9a73af3fa76b4b64a7151360732d236f
imamfarisi19 <edwardindo1@gmail.com>
2021-07-10 02:59:00 +0800

    Generator.return(value) finishes the generator execution and return the given value

---
+++ 12-generators-advanced-iteration/12-1-generators/12-1-7-generator-return/return-0.js
@@ -0,0 +1,12 @@
+function* gen() {
+	yield 1;
+	yield 2;
+	yield 3; 
+} 
+
+const g = gen(); 
+
+g.next();	// { value: 1, done: false } 
+g.return('foo'); // { value: "foo", done: true }
+g.next(); // { value: undefined, done: true } 
+
