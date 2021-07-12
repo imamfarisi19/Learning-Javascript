b84b621909a9883ed8bc0b69fe60f7459fae700a
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 17:24:52 +0800

    As the actual exported functionality is scattered among the package, we can import it into [auth/index.js] and export from it

---
+++ 13-modules/13-2-export-and-import/13-2-7-re-export/re-export-2.js
@@ -0,0 +1,9 @@
+// import login/logout and immediately export them 
+import {login, logout} from './helpers.js'; 
+export {login, logout}; 
+
+// import default as User export it 
+import User from './user.js'; 
+export {User}; 
+... 
+
