08eabfb417c3bc687c34b2ff73a86ac7927ff3fa
imamfarisi19 <edwardindo1@gmail.com>
2021-07-12 14:30:28 +0800

    We can label any declarations as exported by placing export before it, be it a variable, function or a class

---
+++ 13-modules/13-2-export-and-import/13-2-1-export-before-declarations/before-declarations-0.js
@@ -0,0 +1,13 @@
+// export an array 
+export let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+// export a constant 
+export const MODULES_BECAME_STANDARD_YEAR = 2015; 
+
+// export a class 
+export class User {
+	construcor(name) {
+		this.name = name; 
+	}
+}  
+
