9e5713b5ab17cbdd8127ff2e2edc52f0cb109571
imamfarisi19 <edwardindo1@gmail.com>
2021-07-17 20:54:46 +0800

    Once again, the solution with binding the method makes it work

---
+++ 14-miscellaneous/14-1-proxy-and-replect/14-1-9-proxy-limitations/14-1-9-0-built-in-objects-internal-slots/14-1-9-1-private-fields/private-1.js
@@ -0,0 +1,19 @@
+class User {
+	#name = "Guest";
+
+	getName() {
+		return this.#name;
+	}
+} 
+
+let user = new User(); 
+
+user = new Proxy(user, {
+	get(target, prop, receiver) {
+		let value = Reflect.get(...arguments);
+		return typeof value == 'function' ? value.bind(target) : value; 
+	}
+}); 
+
+alert(user.getName()); // Guest 
+
