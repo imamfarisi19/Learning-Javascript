0c17ead6740d4013594e246b685fb4862a913e9c
imamfarisi19 <edwardindo1@gmail.com>
2021-07-17 18:20:51 +0800

    Here's the corrected variant

---
+++ 14-miscellaneous/14-1-proxy-and-replect/14-1-8-reflect/14-1-8-1-proxiying-a-getter/a-getter-2.js
@@ -0,0 +1,20 @@
+let user = {
+	_name: "Guest", 
+	get name() {
+		return this._name; 
+	}
+}; 
+
+let userProxy = new Proxy(user, {
+	get(target, prop, receiver) { // receiver = admin 
+		return Reflect.get(target, prop, receiver); // (*)
+	}
+}); 
+
+let admin = {
+	__proto__: userProxy, 
+	_name: "Admin"
+}; 
+
+alert(admin.name); // Admin 
+
