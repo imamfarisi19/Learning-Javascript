b5e8bb697aee12b718a784ce254c4836544f657c
imamfarisi19 <edwardindo1@gmail.com>
2021-07-17 18:20:50 +0800

    After inheriting another object [admin] from [user], we can observe the incorrect behavior

---
+++ 14-miscellaneous/14-1-proxy-and-replect/14-1-8-reflect/14-1-8-1-proxiying-a-getter/a-getter-1.js
@@ -0,0 +1,21 @@
+let user = {
+	_name: "Guest",
+	get name() {
+		return this._name;
+	}
+}; 
+
+let userProxy = new Proxy(user, {
+	get(target, prop, receiver) {
+		return target[prop]; // (*) target = user 
+	}
+}); 
+
+let admin = {
+	__proto__: userProxy, 
+	_name: "Admin"
+}; 
+
+// Expected: Admin 
+alert(admin.name); // outputs: Guest (?!?)
+
