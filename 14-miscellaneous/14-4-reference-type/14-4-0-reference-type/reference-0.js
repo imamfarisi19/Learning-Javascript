db69340cc9af54f7e7edd6a32636d8a068c4a7c7
imamfarisi19 <edwardindo1@gmail.com>
2021-07-19 18:18:56 +0800

    A dynamically evaluated method call can lose this

---
+++ 14-miscellaneous/14-4-reference-type/14-4-0-reference-type/reference-0.js
@@ -0,0 +1,11 @@
+let user = {
+	name: "John", 
+	hi() { alert(this.name); }, 
+	bye() { alert("Bye"); }
+}; 
+
+user.hi(); // works 
+
+// now let's call user.hi or user.bye depending on the name 
+(user.name == "John" ? user.hi : user.bye) (); // Error! 
+
