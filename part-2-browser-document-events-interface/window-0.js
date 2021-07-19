a2d7ec9f8fe0787e76564c53b3e98b9c56f47627
imamfarisi19 <edwardindo1@gmail.com>
2021-07-20 00:18:26 +0800

    renamed

--- part-2-browser-document-events-interface/window-0.js
+++
@@ -1,7 +0,0 @@
-function sayHi() {
-	alert("Hello");
-}
-
-// global functions are methods of the global object:
-window.sayHi();
-
--- part-2-browser-document-events-interface/window-1.js
+++
@@ -1,2 +0,0 @@
-alert(window.innerHeight) // inner window height 
-
