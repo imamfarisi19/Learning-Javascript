75667b5057412095f692e941c0574551409926ea
imamfarisi19 <edwardindo1@gmail.com>
2021-07-19 14:38:09 +0800

    The new curry may look complicated, but it's actually easy to understand

---
+++ 14-miscellaneous/14-3-currying/14-3-2-advanced-curry-implementation/advanced-2.js
@@ -0,0 +1,11 @@
+// func is the function to transform 
+function curried(...args) { 
+	if(args.length >= func.length) { // (1) 
+		return func.apply(this, args); 
+	} else { 
+		return function(...args2) { // (2) 
+			return curried.apply(this, args.concat(args2));
+		} 
+	} 
+}; 
+
