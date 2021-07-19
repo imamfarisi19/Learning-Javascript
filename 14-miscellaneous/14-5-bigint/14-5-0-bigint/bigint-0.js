053e27f5bc48945b312641be66b1adb3a6b3a937
imamfarisi19 <edwardindo1@gmail.com>
2021-07-19 20:02:10 +0800

    A bigint is created by appending [n] to the end of an integer literal or by calling the function [BigInt] that creates bigints from strings number etc

---
+++ 14-miscellaneous/14-5-bigint/14-5-0-bigint/bigint-0.js
@@ -0,0 +1,6 @@
+const bigint = 1234567890123456789012345678901234567890n;
+
+const sameBigint = BigInt("1234567890123456789012345678901234567890"); 
+
+const bigintFromNumber = BigInt(10); // same as 10n 
+
