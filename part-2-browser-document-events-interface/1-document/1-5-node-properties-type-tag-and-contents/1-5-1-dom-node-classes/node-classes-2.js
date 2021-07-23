4c40caa85a7d82c4020e578c1e7c559356a00542
imamfarisi19 <edwardindo1@gmail.com>
2021-07-24 01:44:39 +0800

    We also can use [instanceof] to check the inheritance

---
+++ part-2-browser-document-events-interface/1-document/1-5-node-properties-type-tag-and-contents/1-5-1-dom-node-classes/node-classes-2.js
@@ -0,0 +1,6 @@
+alert( document.body instanceof HTMLBodyElement ); // true 
+alert( document.body instanceof HTMLElement ); // true 
+alert( document.body instanceof Element ); // true 
+alert( document.body instanceof Node ); // true 
+alert( document.body instanceof EventTarget ); // true 
+
