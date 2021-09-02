b3b12d93440ad0d2dc7282bccfd5990875a9e795
imamfarisi19 <edwardindo1@gmail.com>
2021-09-02 21:18:49 +0800

    To get a name of the class as a string, we can use [constructor.name]. Let's do it for the whole [document] prototype chain, till class [Node]

---
+++ part-2-browser-document-events-interface/1-document/1-5-node-properties-type-tag-and-contents/1-5-10-Tasks/1-5-10-2-wheres-the-document-in-the-hierarchy/wheres-the-document-3.js
@@ -0,0 +1,4 @@
+alert(HTMLDocument.prototype.constructor.name); // HTMLDocument 
+alert(HTMLDocument.prototype.__proto__.constructor.name); // Document 
+alert(HTMLDocument.prototype.__proto__.__proto__.constructor.name); // Node 
+
